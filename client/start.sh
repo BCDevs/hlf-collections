3a6e5965a12b38b99c204899de2a961bea4a48ba
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-30 17:06:21 +0530

    Update start.sh

--- client/start.sh
+++ client/start.sh
@@ -16,11 +16,10 @@ docker-compose -f ./docker-compose.yml up -d cli
 docker ps -a
 
 
-
-# Install chaincode
+echo 'Installing chaincode..'
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/newcc" -l "node"
 
-# Instanitate chaincode
+echo 'Instanitating chaincode..'
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -l "node" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 
 echo 'Getting things ready for Chaincode Invocation..should take only 10 seconds..'
