e9a040fc4fc898e71c3ec91a562afca2f3c31ec2
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-02 23:11:23 +0530

    Update start.sh

--- client/start.sh
+++ client/start.sh
@@ -1,23 +1,43 @@
+cd ..
+git clone https://github.com/Salmandabbakuti/hlf-testnet.git
+
+
 echo "Removing key from key store..."
 
-rm -rf ./hfc-key-store
+rm -rf hfc-key-store
 
 
 # Remove chaincode docker image
-docker rmi -f dev-peer0.org1.example.com-mycc-1.0-384f11f484b9302df90b453200cfb25174305fce8f53f4e94d45ee3b6cab0ce9
 
+STOP AND DELETE THE DOCKER CONTAINERS
+docker ps -aq | xargs -n 1 docker stop
+docker ps -aq | xargs -n 1 docker rm -v
 
-cd ../basic-network
+# DELETE THE OLD DOCKER VOLUMES
+docker volume prune
+
+# DELETE OLD DOCKER NETWORKS (OPTIONAL: seems to restart fine without)
+docker network prune
+
+echo 'Mounting chaincode..'
+cd hlf-testnet
+rm -rf chaincode
+cd ..
+cp -r chaincode hlf-testnet
+
+cd hlf-testnet
+
 ./start.sh
 
-# Now launch the CLI container in order to install, instantiate chaincode
-# and prime the ledger with our 10 cars
-docker-compose -f ./docker-compose.yml up -d cli
 docker ps -a
 
 
 echo 'Installing chaincode..'
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/chaincode/newcc" -l "node"
+docker exec -it cli peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/chaincode/newcc" -l "node"
+
+docker exec -it cl2 peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/chaincode/newcc" -l "node"
+docker exec -it cli3 peer chaincode install -n mycc -v 1.0 -p "/opt/gopath/src/github.com/chaincode/newcc" -l "node"
+
 
 echo 'Instanitating chaincode..'
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc -l "node" -v 1.0 -c '{"Args":[""]}' --collections-config "/opt/gopath/src/github.com/chaincode/newcc/collection-config.json" -P "OR ('Org1MSP.member','Org2MSP.member')"
@@ -36,11 +56,15 @@ docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -C mychann
 sleep 3
 echo 'Querying Public Product..'
 
-docker exec -e “CORE_PEER_LOCALMSPID=Org1MSP” -e “CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp” cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c '{"function":"queryProduct","Args":["publicCollection","MSFTP3"]}'
+docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c '{"function":"queryProduct","Args":["publicCollection","MSFTP3"]}'
 
-echo 'Querying Private Product..'
+echo 'Querying on Org1 Peer  Private Product..'
 
-docker exec -e “CORE_PEER_LOCALMSPID=Org1MSP” -e “CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp” cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c '{"function":"queryProduct","Args":["privateCollection","MSFTP3V"]}'
+docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c '{"function":"queryProduct","Args":["privateCollection","MSFTP3V"]}'
+
+echo 'Querying on Org2 Peer  Private Product..'
+
+docker exec -it cli2 peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n mycc -c '{"function":"queryProduct","Args":["privateCollection","MSFTP3V"]}'
 
 
 # Starting docker logs of chaincode container
